1293c64f3b85da371104cf48dd5e4a3114af9204
randysongEXE <127687854+randysongEXE@users.noreply.github.com>
2023-03-15 00:07:04 -0500

    Create PasswordChecker.java

---
+++ PasswordChecker.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+public class PasswordChecker {
+    private static final int SENTINEL = -1;
+
+    //main method reads user input and runs it against boolean methods to check if the password is strong or weak.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter password, or -1 to exit. : ");
+            String password = scanner.nextLine();
+            if (password.equals(String.valueOf(SENTINEL))) {
+                System.out.println("Sentinel value inputted. Exiting...");
+                break;
+            }
+
+
+            boolean isStrong = isStrongPassword(password);
+
+
+            if (isStrong) {
+                System.out.println("Password is strong!");
+            } else
+                System.out.println("Password is weak.");
+
+
+        }
+        scanner.close();
+    }
+
+
+    //This method uses a series of if-statements to check against various methods
+    public static boolean isStrongPassword(String password) {
+
+        ContainsVars cv = new ContainsVars();
+        DictionaryCheck dc = new DictionaryCheck();
+        BruteForceCheck bc = new BruteForceCheck();
+
+
+        //checking various methods
+        if (password.length() <= 8) {
+            return false;
+        }
+        if (!(cv.containsUpperCase(password))) {
+            return false;
+        }
+        if (!(cv.containsLowerCase(password))) {
+            return false;
+        }
+        if (!(cv.containsDigit(password))) {
+            return false;
+        }
+        if (!(cv.containsSpecialChar(password))) {
+            return false;
+        }
+        if (dc.dictionaryCheck(password)) {
+            return false;
+        }
+        return !bc.bruteForceCheck(password);
+    }
+}
+
+
+
+

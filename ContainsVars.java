9519cea87377b22aae9a0072c1ad7ea891bfe508
randysongEXE <127687854+randysongEXE@users.noreply.github.com>
2023-03-15 00:08:13 -0500

    Create ContainsVars.java

---
+++ ContainsVars.java
@@ -0,0 +1,40 @@
+public class ContainsVars {
+
+    //These methods check to see if the password meets basic requirements, such as containing an uppercase, a lowercase, etc.
+
+    public boolean containsUpperCase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsLowerCase(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDigit(String password) {
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsSpecialChar(String password) {
+        for (char c : password.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
